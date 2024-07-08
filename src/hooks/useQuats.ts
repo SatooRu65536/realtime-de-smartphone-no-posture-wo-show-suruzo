799d95ce4c0356a5c3400dc4295db89f25e54e29
satooru65536 <ryuto4619@gmail.com>
2024-07-08 21:16:01 +0900

    style: 修正

--- src/hooks/useQuats.ts
+++ src/hooks/useQuats.ts
@@ -1,33 +1,17 @@
 import { Quat, quatResponseSchema } from '@/schema/sensor';
-import { useArray } from './useArray';
 import { useWS } from './useWS';
-import { useEffect, useMemo, useState } from 'react';
+import { useMemo, useState } from 'react';
 import { Quaternion } from 'three';
 
-export const useQuats = (url: URL, intervalTime = 1000 / 60) => {
-  const [quats, { set }] = useArray<Quat>();
+export const useQuats = (url: URL) => {
   const [quaternion, setQuaternion] = useState<Quat>([0, 0, 0, 0]);
 
   useWS(url.href, (data) => {
     const isValid = quatResponseSchema.safeParse(data);
     if (!isValid.success) return;
 
-    // set(isValid.data.quaternions);
     setQuaternion(isValid.data.quaternions[0]);
   });
-
-  useEffect(() => {
-    const clonedQuats = structuredClone(quats);
-
-    const interval = setInterval(() => {
-      const quat = clonedQuats.pop();
-      if (!quat) return;
-
-      setQuaternion(quat);
-    }, intervalTime);
-
-    return () => clearInterval(interval);
-  }, [quats]);
 
   const qut = useMemo(() => {
     const [x, y, z, w] = quaternion;
