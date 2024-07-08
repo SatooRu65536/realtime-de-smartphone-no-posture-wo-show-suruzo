03420bddacdfcc321f372742db6287b5b63bcbb5
satooru65536 <ryuto4619@gmail.com>
2024-07-08 21:20:07 +0900

    fix

--- src/components/RotateModel.tsx
+++ src/components/RotateModel.tsx
@@ -5,7 +5,7 @@ const { VITE_WS_BASE_URL } = import.meta.env;
 const WS_URL = new URL('/ws/posture', VITE_WS_BASE_URL);
 
 export default function RotateModel() {
-  const quat = useQuats(WS_URL, 1000 / 60);
+  const quat = useQuats(WS_URL);
 
   return <MobileModel src="/models/mobile.glb" quaternion={quat} />;
 }
